6b243a010a5cf0ab5ca0a2b168c46a0123691670
loaki07 <loakesh.i@gmail.com>
2021-08-23 19:47:51 +0530

    Test: bid_for_task with correct details Implemented:
    Because:

--- pallets/pallet-tasking/src/tests.rs
+++ pallets/pallet-tasking/src/tests.rs
@@ -1,7 +1,6 @@
-use frame_system::ensure_signed;
-use crate::{mock::*, Error, TaskDetails, Status};
+use crate::{mock::*, Error, Status, TaskDetails};
 use frame_support::{assert_noop, assert_ok, dispatch::DispatchError};
-
+use frame_system::ensure_signed;
 
 #[test]
 fn throws_error_for_unsigned_origin_while_creating_task_with_correct_() {
@@ -25,17 +24,25 @@ fn it_works_for_creating_a_task_with_correct_details_provided() {
             b"Create a website".to_vec()
         ));
         // Read pallet storage and assert an expected result.
-		let sender = ensure_signed(Origin::signed(1)).unwrap();
-		let expected_task_details = TaskDetails {
-			task_id: 0,
-			publisher: sender.clone(),
-			worker_id: None,
-			task_deadline: 30,
-			cost: 300,
-			status: Status::Open,
-			task_description: b"Create a website".to_vec(),
-		};
+        let sender = ensure_signed(Origin::signed(1)).unwrap();
+        let expected_task_details = TaskDetails {
+            task_id: 0,
+            publisher: sender.clone(),
+            worker_id: None,
+            task_deadline: 30,
+            cost: 300,
+            status: Status::Open,
+            task_description: b"Create a website".to_vec(),
+        };
         assert_eq!(PalletTasking::task(0), expected_task_details);
     });
 }
 
+#[test]
+fn it_works_for_bidding_a_task_with_correct_details() {
+    new_test_ext().execute_with(|| {
+        PalletTasking::create_task(Origin::signed(1), 50, 500, b"Backend Systems".to_vec())
+            .unwrap();
+        assert_ok!(PalletTasking::bid_for_task(Origin::signed(3), 0));
+    })
+}
