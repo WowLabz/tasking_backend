3ec867d9b60ed5078bdab1dea4dd9167f6fbbd2f
Tejas <tejasvijay09@gmail.com>
2022-01-17 19:09:39 +0530

    A: create task extrinsic

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -16,14 +16,74 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
+	// use log::{info, trace, warn};
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
+	use frame_support::{
+		log,
+        sp_runtime::traits::Hash,
+        traits::{ Randomness, Currency, tokens::ExistenceRequirement, LockIdentifier, WithdrawReasons, LockableCurrency },
+		dispatch::{ EncodeLike },
+        transactional
+    };
+	use sp_std::vec::Vec;
+	// use codec::{EncodeLike};
+
+	type AccountOf<T> = <T as frame_system::Config>::AccountId;
+	type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	// NOTE: Need to refactor, duplicate code for testing purposes
+	type AccountId<T> = <T as frame_system::Config>::AccountId;
+	type Balance<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	pub const LOCKSECRET: LockIdentifier = *b"mylockab";
+
+	#[derive(Encode, Decode, PartialEq, Eq, Debug, Clone, TypeInfo)]
+	pub enum TaskTypeTags {
+		WebDevelopment,
+		MobileDevelopment,
+		MachineLearning, 
+		DeepLearning,
+		FullStackDevelopment,
+		CoreBlockchainDevelopment,
+	}
+
+	#[derive(Encode, Decode, PartialEq, Eq, Debug, Clone, TypeInfo)]
+	pub enum Status {
+		Open,
+		InProgress,
+		PendingApproval,
+		PendingRatings,
+		Completed,
+	}
+
+	impl Default for Status {
+		fn default() -> Self {
+			Status::Open
+		}
+	}
+
+	#[derive(Encode, Decode, Default, Debug, PartialEq, Clone, Eq, TypeInfo)]
+	pub struct TaskDetails<AccountId, Balance> {
+		task_id: u128,
+		publisher: AccountId,
+		worker_id: Option<AccountId>,
+		publisher_name: Option<Vec<u8>>,
+		worker_name: Option<Vec<u8>>,
+		task_tags: Vec<TaskTypeTags>,
+		task_deadline: u64,
+		cost: Balance,
+		status: Status,
+		task_description: Vec<u8>,
+		attachments: Option<Vec<Vec<u8>>>
+	}
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		type Currency: LockableCurrency<Self::AccountId>;
 	}
 
 	#[pallet::pallet]
@@ -33,10 +93,14 @@ pub mod pallet {
 	// The pallet's runtime storage items.
 	// https://docs.substrate.io/v3/runtime/storage
 	#[pallet::storage]
-	#[pallet::getter(fn something)]
+	#[pallet::getter(fn get_task_count)]
 	// Learn more about declaring storage items:
 	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
+	pub type TaskCount<T> = StorageValue<_, u128, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn task)]
+	pub(super) type TaskStorage<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, TaskDetails<T::AccountId, BalanceOf<T>>, ValueQuery>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/v3/runtime/events-and-errors
@@ -46,6 +110,7 @@ pub mod pallet {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
 		SomethingStored(u32, T::AccountId),
+		TaskCreated(T::AccountId, Vec<u8>, u128, u64, BalanceOf<T>, Vec<u8>)
 	}
 
 	// Errors inform users that something went wrong.
@@ -62,41 +127,100 @@ pub mod pallet {
 	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
 
-			// Update storage.
-			<Something<T>>::put(something);
-
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
+		#[pallet::weight(10_000)]
+		pub fn create_task(
+			origin: OriginFor<T>, 
+			task_duration: u64, 
+			task_cost: BalanceOf<T>, 
+			task_des: Vec<u8>, 
+			publisher_name: Vec<u8>, 
+			task_tags: Vec<TaskTypeTags>, 
+			publisher_attachments: Option<Vec<Vec<u8>>>
+		) -> DispatchResult {
+			let who = ensure_signed(origin).unwrap();
+			let current_task_count = Self::get_task_count();
+			log::info!("$$$$$ Current task count: {:#?}", current_task_count);
+			let result_from_locking = T::Currency::set_lock(LOCKSECRET, &who, task_cost.clone(), WithdrawReasons::TRANSACTION_PAYMENT);
+			log::info!("$$$$$ Locked amount of publisher: {:#?}", result_from_locking);
+			let task_details = TaskDetails {
+				task_id: current_task_count.clone(),
+				publisher: who.clone(),
+				worker_id: None,
+				publisher_name: Some(publisher_name.clone()),
+				worker_name: None,
+				task_tags: task_tags.clone(),
+				task_deadline: task_duration.clone(),
+				cost:task_cost.clone(),
+				status: Default::default(),
+				task_description: task_des.clone(),
+				attachments: publisher_attachments.clone(),
+			};
+			log::info!("$$$$$ Task details: {:#?}", task_details);
+			TaskStorage::<T>::insert(<T as EncodeLike>::current_task_count.clone(), <T as EncodeLike>::task_details);
+			Self::deposit_event(
+				Event::TaskCreated(
+					who, 
+					publisher_name.clone(), 
+					current_task_count.clone(), 
+					task_duration.clone(), 
+					task_cost.clone(), 
+					task_des.clone()
+				)
+			);
+			TaskCount::put(current_task_count + 1);
 			Ok(())
 		}
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+		// #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		// pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+		// 	let who = ensure_signed(origin)?;
+		// 	<Something<T>>::put(something);
+		// 	Self::deposit_event(Event::SomethingStored(something, who));
+		// 	Ok(())
+		// }
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => Err(Error::<T>::NoneValue)?,
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
+		// #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
+		// pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
+		// 	let _who = ensure_signed(origin)?;
+
+		// 	match <Something<T>>::get() {
+		// 		None => Err(Error::<T>::NoneValue)?,
+		// 		Some(old) => {
+		// 			let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+		// 			<Something<T>>::put(new);
+		// 			Ok(())
+		// 		},
+		// 	}
+		// }
 	}
 }
+
+/*
+#[weight = 10_000]
+pub fn create_task(origin, task_duration: u64, task_cost: BalanceOf<T>, task_des: Vec<u8>, publisher_name: Vec<u8>, task_tags: Vec<TaskTypeTags>, publisher_attachments: Option<Vec<Vec<u8>>>) {
+	let sender = ensure_signed(origin)?;
+	let current_count = Self::get_task_count();
+
+	let result_from_locking = T::Currency::set_lock(LOCKSECRET, &sender, task_cost.clone(), WithdrawReasons::TRANSACTION_PAYMENT);
+	debug::info!("result_from_locking : {:#?}", result_from_locking);
+
+	let temp= TaskDetails {
+		task_id: current_count.clone(),
+		publisher: sender.clone(),
+		worker_id: None,
+		publisher_name: Some(publisher_name.clone()),
+		worker_name: None,
+		task_tags: task_tags.clone(),
+		task_deadline: task_duration.clone(),
+		cost:task_cost.clone(),
+		status: Default::default(),
+		task_description: task_des.clone(),
+		attachments: publisher_attachments.clone(),
+	};
+
+	TaskStorage::<T>::insert(current_count.clone(), temp);
+	Self::deposit_event(RawEvent::TaskCreated(sender, publisher_name.clone(), current_count.clone(), task_duration.clone(), task_cost.clone(), task_des.clone()));
+	TaskCount::put(current_count + 1);
+}
+*/
