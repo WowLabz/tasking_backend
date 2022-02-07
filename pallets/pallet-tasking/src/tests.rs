2aa4b3e08fc711f4713402af7fedcc72e0a47d10
Praneeth <praneethar2000@gmail.com>
2022-02-07 11:07:18 +0530

    Add: test.rs for pallet tasking

--- pallets/pallet-tasking/src/tests.rs
+++ pallets/pallet-tasking/src/tests.rs
@@ -1,20 +1,32 @@
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok};
+use crate::mock::ExtBuilder;
+use crate::{mock::*, Error, Status, TaskDetails, TaskTypeTags};
+use frame_support::{assert_noop, assert_ok, dispatch::DispatchError};
+use frame_system::{ensure_signed, RawOrigin};
 
 #[test]
-fn it_works_for_default_value() {
+fn test_create_task (){
 	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(Origin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-	});
+
+	})
+
 }
 
-#[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(TemplateModule::cause_error(Origin::signed(1)), Error::<Test>::NoneValue);
-	});
+fn correct_error_for_unsigned_origin_while_creating_task_with_correct_() {
+    new_test_ext().execute_with(|| {
+        // Ensure the expected error is thrown when no value is present.
+        assert_noop!(
+            Tasking::create_task(
+                Origin::none(),
+                30,
+                300,
+                b"Create a website".to_vec(),
+                b"Alice".to_vec(),
+                vec![TaskTypeTags::WebDevelopment],
+                Some(vec![b"http://aws/publisher.png".to_vec()])
+            ),
+            DispatchError::BadOrigin,
+        );
+    });
 }
+
+
